41441f38f026b8ca315a3ae7b2b0a00880244a4b
Jeffrey Walter <jeffreydwalter@gmail.com>
2024-10-15 10:38:36 -0500

    fix-many-to-many-panic: Fix map key hashing func

--- internal/map_key.go
+++ internal/map_key.go
@@ -1,6 +1,10 @@
 package internal
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 var ifaceType = reflect.TypeOf((*interface{})(nil)).Elem()
 
@@ -15,53 +19,12 @@ func NewMapKey(is []interface{}) MapKey {
 }
 
 func newMapKey(is []interface{}) interface{} {
-	switch len(is) {
-	case 1:
-		ptr := new([1]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 2:
-		ptr := new([2]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 3:
-		ptr := new([3]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 4:
-		ptr := new([4]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 5:
-		ptr := new([5]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 6:
-		ptr := new([6]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 7:
-		ptr := new([7]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 8:
-		ptr := new([8]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 9:
-		ptr := new([9]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	case 10:
-		ptr := new([10]interface{})
-		copy((*ptr)[:], is)
-		return *ptr
-	default:
-	}
-
-	at := reflect.New(reflect.ArrayOf(len(is), ifaceType)).Elem()
+	var sb strings.Builder
 	for i, v := range is {
-		*(at.Index(i).Addr().Interface().(*interface{})) = v
+		if i > 0 {
+			sb.WriteString("|")
+		}
+		sb.WriteString(fmt.Sprintf("%v", v))
 	}
-	return at.Interface()
+	return sb.String()
 }
